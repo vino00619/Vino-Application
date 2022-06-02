41212b4799d8588b058fc72d9209de072fe63788
vino00619 <vinoth00619@gmail.com>
2022-06-02 15:23:06 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
-import { NavigateBeforeRounded } from '@material-ui/icons';
-import Navbar from './components/Navbar';
+//import { NavigateBeforeRounded } from '@material-ui/icons';
+//import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
